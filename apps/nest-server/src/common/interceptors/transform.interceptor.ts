60976caf05dcec4913251a36ff7f22e985341d3c
mizxinp <mizxinp@gmail.com>
2023-05-07 23:38:33 +0800

    feat[nest]: add TransformInterceptor

---
+++ apps/nest-server/src/common/interceptors/transform.interceptor.ts
@@ -0,0 +1,31 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpStatus } from 'src/config/httpStatus';
+
+interface Response<T> {
+  result: T;
+}
+
+@Injectable()
+export class TransformInterceptor<T>
+  implements NestInterceptor<T, Response<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((result) => ({
+        result,
+        code: HttpStatus.SUCCESS,
+        message: 'success',
+      })),
+    );
+  }
+}
